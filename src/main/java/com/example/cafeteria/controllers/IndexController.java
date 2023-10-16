dbbea144f6f942a1d5ba1788ce7c6db9f74bc10b
Weuller Oliveira <weullerwww@gmail.com>
2023-10-16 14:22:42 -0300

    Criado controller para pagina index e add pagina index

---
+++ src/main/java/com/example/cafeteria/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.cafeteria.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home/index");
+        return mv;
+    }
+
+}
